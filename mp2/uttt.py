cf05b1c6ee5ec0b82e0bfa5ab5093089f93228c7
Xavier Chi <chitianhaoxp@gmail.com>
2019-02-20 15:29:17 -0600

    some temporary update

--- mp2/uttt.py
+++ mp2/uttt.py
@@ -2,52 +2,56 @@ from time import sleep
 from math import inf
 from random import randint
 
+
 class ultimateTicTacToe:
     def __init__(self):
         """
         Initialization of the game.
         """
-        self.board=[['_','_','_','_','_','_','_','_','_'],
-                    ['_','_','_','_','_','_','_','_','_'],
-                    ['_','_','_','_','_','_','_','_','_'],
-                    ['_','_','_','_','_','_','_','_','_'],
-                    ['_','_','_','_','_','_','_','_','_'],
-                    ['_','_','_','_','_','_','_','_','_'],
-                    ['_','_','_','_','_','_','_','_','_'],
-                    ['_','_','_','_','_','_','_','_','_'],
-                    ['_','_','_','_','_','_','_','_','_']]
-        self.maxPlayer='X'
-        self.minPlayer='O'
-        self.maxDepth=3
-        #The start indexes of each local board
-        self.globalIdx=[(0,0),(0,3),(0,6),(3,0),(3,3),(3,6),(6,0),(6,3),(6,6)]
+        self.board = [['_', '_', '_', '_', '_', '_', '_', '_', '_'],
+                      ['_', '_', '_', '_', '_', '_', '_', '_', '_'],
+                      ['_', '_', '_', '_', '_', '_', '_', '_', '_'],
+                      ['_', '_', '_', '_', '_', '_', '_', '_', '_'],
+                      ['_', '_', '_', '_', '_', '_', '_', '_', '_'],
+                      ['_', '_', '_', '_', '_', '_', '_', '_', '_'],
+                      ['_', '_', '_', '_', '_', '_', '_', '_', '_'],
+                      ['_', '_', '_', '_', '_', '_', '_', '_', '_'],
+                      ['_', '_', '_', '_', '_', '_', '_', '_', '_']]
+        self.maxPlayer = 'X'
+        self.minPlayer = 'O'
+        self.maxDepth = 3
+        # The start indexes of each local board
+        self.globalIdx = [(0, 0), (0, 3), (0, 6), (3, 0),
+                          (3, 3), (3, 6), (6, 0), (6, 3), (6, 6)]
 
-        #Start local board index for reflex agent playing
-        self.startBoardIdx=4
-        #self.startBoardIdx=randint(0,8)
+        # Start local board index for reflex agent playing
+        self.startBoardIdx = 4
+        # self.startBoardIdx=randint(0,8)
 
-        #utility value for reflex offensive and reflex defensive agents
-        self.winnerMaxUtility=10000
-        self.twoInARowMaxUtility=500
-        self.preventThreeInARowMaxUtility=100
-        self.cornerMaxUtility=30
+        # utility value for reflex offensive and reflex defensive agents
+        self.winnerMaxUtility = 10000
+        self.twoInARowMaxUtility = 500
+        self.preventThreeInARowMaxUtility = 100
+        self.cornerMaxUtility = 30
 
-        self.winnerMinUtility=-10000
-        self.twoInARowMinUtility=-100
-        self.preventThreeInARowMinUtility=-500
-        self.cornerMinUtility=-30
+        self.winnerMinUtility = -10000
+        self.twoInARowMinUtility = -100
+        self.preventThreeInARowMinUtility = -500
+        self.cornerMinUtility = -30
 
-        self.expandedNodes=0
-        self.currPlayer=True
+        self.expandedNodes = 0
+        self.currPlayer = True
 
     def printGameBoard(self):
         """
         This function prints the current game board.
         """
-        print('\n'.join([' '.join([str(cell) for cell in row]) for row in self.board[:3]])+'\n')
-        print('\n'.join([' '.join([str(cell) for cell in row]) for row in self.board[3:6]])+'\n')
-        print('\n'.join([' '.join([str(cell) for cell in row]) for row in self.board[6:9]])+'\n')
-
+        print('\n'.join([' '.join([str(cell) for cell in row])
+                         for row in self.board[:3]])+'\n')
+        print('\n'.join([' '.join([str(cell) for cell in row])
+                         for row in self.board[3:6]])+'\n')
+        print('\n'.join([' '.join([str(cell) for cell in row])
+                         for row in self.board[6:9]])+'\n')
 
     def evaluatePredifined(self, isMax):
         """
@@ -58,10 +62,9 @@ class ultimateTicTacToe:
         output:
         score(float): estimated utility score for maxPlayer or minPlayer
         """
-        #YOUR CODE HERE
-        score=0
+        # YOUR CODE HERE
+        score = 0
         return score
-
 
     def evaluateDesigned(self, isMax):
         """
@@ -72,8 +75,8 @@ class ultimateTicTacToe:
         output:
         score(float): estimated utility score for maxPlayer or minPlayer
         """
-        #YOUR CODE HERE
-        score=0
+        # YOUR CODE HERE
+        score = 0
         return score
 
     def checkMovesLeft(self):
@@ -83,12 +86,12 @@ class ultimateTicTacToe:
         movesLeft(bool): boolean variable indicates whether any legal move remains
                         on the board.
         """
-        #YOUR CODE HERE
-        movesLeft=True
+        # YOUR CODE HERE
+        movesLeft = True
         return movesLeft
 
     def checkWinner(self):
-        #Return termimnal node status for maximizer player 1-win,0-tie,-1-lose
+        # Return termimnal node status for maximizer player 1-win,0-tie,-1-lose
         """
         This function checks whether there is a winner on the board.
         output:
@@ -96,11 +99,11 @@ class ultimateTicTacToe:
                      Return 1 if maxPlayer is the winner.
                      Return -1 if miniPlayer is the winner.
         """
-        #YOUR CODE HERE
-        winner=0
+        # YOUR CODE HERE
+        winner = 0
         return 0
 
-    def alphabeta(self,depth,currBoardIdx,alpha,beta,isMax):
+    def alphabeta(self, depth, currBoardIdx, alpha, beta, isMax):
         """
         This function implements alpha-beta algorithm for ultimate tic-tac-toe game.
         input args:
@@ -113,8 +116,8 @@ class ultimateTicTacToe:
         output:
         bestValue(float):the bestValue that current player may have
         """
-        #YOUR CODE HERE
-        bestValue=0.0
+        # YOUR CODE HERE
+        bestValue = 0.0
         return bestValue
 
     def minimax(self, depth, currBoardIdx, isMax):
@@ -130,11 +133,11 @@ class ultimateTicTacToe:
         output:
         bestValue(float):the bestValue that current player may have
         """
-        #YOUR CODE HERE
-        bestValue=0.0
+        # YOUR CODE HERE
+        bestValue = 0.0
         return bestValue
 
-    def playGamePredifinedAgent(self,maxFirst,isMinimaxOffensive,isMinimaxDefensive):
+    def playGamePredifinedAgent(self, maxFirst, isMinimaxOffensive, isMinimaxDefensive):
         """
         This function implements the processes of the game of predifined offensive agent vs defensive agent.
         input args:
@@ -151,11 +154,11 @@ class ultimateTicTacToe:
         gameBoards(list of 2d lists): list of game board positions at each move
         winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
         """
-        #YOUR CODE HERE
-        bestMove=[]
-        bestValue=[]
-        gameBoards=[]
-        winner=0
+        # YOUR CODE HERE
+        bestMove = []
+        bestValue = []
+        gameBoards = []
+        winner = 0
         return gameBoards, bestMove, expandedNodes, bestValue, winner
 
     def playGameYourAgent(self):
@@ -167,12 +170,11 @@ class ultimateTicTacToe:
         gameBoards(list of 2d lists): list of game board positions at each move
         winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
         """
-        #YOUR CODE HERE
-        bestMove=[]
-        gameBoards=[]
-        winner=0
+        # YOUR CODE HERE
+        bestMove = []
+        gameBoards = []
+        winner = 0
         return gameBoards, bestMove, winner
-
 
     def playGameHuman(self):
         """
@@ -182,15 +184,17 @@ class ultimateTicTacToe:
         gameBoards(list of 2d lists): list of game board positions at each move
         winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
         """
-        #YOUR CODE HERE
-        bestMove=[]
-        gameBoards=[]
-        winner=0
+        # YOUR CODE HERE
+        bestMove = []
+        gameBoards = []
+        winner = 0
         return gameBoards, bestMove, winner
 
-if __name__=="__main__":
-    uttt=ultimateTicTacToe()
-    gameBoards, bestMove, bestValue, winner=uttt.playGameReflexAgent()
+
+if __name__ == "__main__":
+    uttt = ultimateTicTacToe()
+    gameBoards, bestMove, expandedNodes, bestValue, winner = uttt.playGamePredifinedAgent(
+        True, False, False)
     if winner == 1:
         print("The winner is maxPlayer!!!")
     elif winner == -1:
